c18663dcf520764cf490f100a82fc0128bbff2ec
Donghee <dongh94@naver.com>
2023-08-11 16:00:46 +0900

    backend User-Login,Join

---
+++ Backend/src/main/java/com/fintech/moang/MoangApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.moang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoangApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/fintech/moang/MoangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.moang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
